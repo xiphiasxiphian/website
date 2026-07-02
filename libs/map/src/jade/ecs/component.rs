48543c95d44e91bb2ffb8a3e065fbfb2df328ed5
Sophie Holman <siaphix.siaphix@gmail.com>
2026-07-02 23:57:49 +0100

    basic component trait

---
+++ libs/map/src/jade/ecs/component.rs
@@ -0,0 +1,19 @@
+use std::any::Any;
+
+use crate::input::InputState;
+
+pub trait Component: Any
+{
+    fn start(&mut self, _ctx: ComponentContext) {}
+
+    fn tick(&mut self, _ctx: ComponentContext, _dt: f32) {}
+
+    // downcasting
+    fn as_any(&self) -> &dyn Any;
+    fn as_any_mut(&mut self) -> &mut dyn Any;
+}
+
+pub struct ComponentContext<'a>
+{
+    pub input: &'a InputState,
+}
