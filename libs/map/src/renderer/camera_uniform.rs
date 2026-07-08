dc82a72d883f7296082fd8a2428918e9b52894da
Sophie Holman <siaphix.siaphix@gmail.com>
2026-07-08 23:02:46 +0100

    camera_uniform

---
+++ libs/map/src/renderer/camera_uniform.rs
@@ -0,0 +1,67 @@
+use glam::Mat4;
+use wgpu::{BindGroup, BindGroupDescriptor, BindGroupEntry, BindGroupLayout, BindGroupLayoutDescriptor, BindGroupLayoutEntry, BindingType, Buffer, BufferBindingType, BufferUsages, Device, ShaderStages, util::{BufferInitDescriptor, DeviceExt}};
+
+#[repr(C)]
+#[derive(Clone, Copy, bytemuck::Pod, bytemuck::Zeroable)]
+pub struct CameraUniform
+{
+    view_projection: [[f32; 4]; 4],
+}
+
+impl CameraUniform
+{
+    const IDENTITY: Self = Self::from_matrix(Mat4::IDENTITY);
+
+    pub const fn from_matrix(matrix: Mat4) -> Self
+    {
+        Self { view_projection: matrix.to_cols_array_2d() }
+    }
+}
+
+pub struct CameraBuffer
+{
+    pub buffer: Buffer,
+    pub bind_group: BindGroup,
+    pub layout: BindGroupLayout,
+}
+
+impl CameraBuffer
+{
+    pub fn new(device: &Device) -> Self
+    {
+        let layout = device.create_bind_group_layout(&BindGroupLayoutDescriptor {
+            label: Some("camera_bind_group_layout"),
+            entries: &[BindGroupLayoutEntry {
+                binding: 0,
+                visibility: ShaderStages::VERTEX,
+                ty: BindingType::Buffer {
+                    ty: BufferBindingType::Uniform,
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            }],
+        });
+
+        let buffer = device.create_buffer_init(&BufferInitDescriptor {
+            label: Some("camera_buffer"),
+            contents: bytemuck::bytes_of(&CameraUniform::IDENTITY),
+            usage: BufferUsages::UNIFORM | BufferUsages::COPY_DST,
+        });
+
+        let bind_group = device.create_bind_group(&BindGroupDescriptor {
+            label: Some("camera_bind_group"),
+            layout: &layout,
+            entries: &[BindGroupEntry {
+                binding: 0,
+                resource: buffer.as_entire_binding(),
+            }],
+        });
+
+        Self {
+            buffer,
+            bind_group,
+            layout,
+        }
+    }
+}
