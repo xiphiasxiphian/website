b59c8accfdece0e9445cb0a11d0d47ad3d0a1a54
Sophie Holman <siaphix.siaphix@gmail.com>
2026-07-12 21:43:09 +0100

    sound handler for playing audio

---
+++ libs/map/src/jade/audio/mod.rs
@@ -0,0 +1,37 @@
+use kira::{manager::{AudioManager, AudioManagerSettings, DefaultBackend, backend::Backend}, sound::static_sound::{StaticSoundData, StaticSoundHandle, StaticSoundSettings}};
+
+pub type Sound = StaticSoundData;
+
+pub struct SoundHandler
+{
+    manager: AudioManager
+}
+
+impl SoundHandler
+{
+    pub fn new() -> Result<Self, <DefaultBackend as Backend>::Error>
+    {
+        Ok(Self {
+            manager: AudioManager::<DefaultBackend>::new(
+                AudioManagerSettings::default(),
+            )?,
+        })
+    }
+
+    pub fn play(&mut self, sound: &Sound)
+        -> Result<StaticSoundHandle, kira::manager::error::PlaySoundError<()>>
+    {
+        self.manager.play(sound.clone())
+    }
+
+    pub fn play_with(&mut self, sound: &Sound, settings: StaticSoundSettings)
+        -> Result<StaticSoundHandle, kira::manager::error::PlaySoundError<()>>
+    {
+        self.manager.play(
+            sound
+                .clone()
+                .with_settings(settings)
+        )
+    }
+
+}
